0c7de1177cf65da290adfc0164103e515377d1ca
sibuyi cs <comfortsiyabonga24@gmail.com>
2023-06-01 20:04:13 +0200

    final changes

--- Script.js
+++ Script.js
@@ -1,76 +1,18 @@
-body{
-    position: relative;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    
+const wrapper=document.querySelector('.wrapper');
+const loginLink=document.querySelector('.login-link');
+const registerLink=document.querySelector('.register-link');
+const btnPopup=document.querySelector('.btnLogin-popup');
+const btnClose=document.querySelector('.close_icon');
 
-    
-}
-.header{
-
-    width: 100%;
-    height: 50px;
-    position: relative;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    
-}
-.logo{
-    margin-right: auto;
-    padding-left: 50px;
-}
-
-a{
-    text-decoration: none;
-    color: white;
-    padding-left: 20px;
-   
-}
-.nav{
-
-}
-.nav button{
-    
-    margin-right: 50px;
-    background: transparent;
-    border: 2px solid white;
-    border-radius: 5px;
-    padding-left:20px;
-    padding-right: 20px;
-    padding-top: 5px;
-    color: white;
-    
-}
-.regitration{
-    display: none;
-}
-
-
-form{
-   
-    padding: 40px;
-    border-radius: 15px;
-    background-color: rgba(107, 107, 131, 0.555);
-    
-
-}
-.inputs-text{
-    display:grid;
-    gap: 10px;
-}
-
-input{
-    background: transparent;
-    border-bottom: 5px solid black;
-}
-
-form button{
-    background: rgb(3, 3, 39);
-    color: white;
-    width: 100%;
-    border-radius: 10px;
-    height: 40px;
-    font-weight: 600;
-}
+registerLink.addEventListener('click',()=>{
+    wrapper.classList.add('active');
+});
+loginLink.addEventListener('click',()=>{
+    wrapper.classList.remove('active');
+});
+btnPopup.addEventListener('click',()=>{
+    wrapper.classList.add('active-popup');
+});
+btnClose.addEventListener('click',()=>{
+    wrapper.classList.remove('active-popup');
+});
