e5d03a46f08727f45d5e3f891403aa446028f7bf
sibuyi cs <comfortsiyabonga24@gmail.com>
2023-06-01 17:56:06 +0200

    adding files from other folders

---
+++ Script.js
@@ -0,0 +1,76 @@
+body{
+    position: relative;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    
+
+    
+}
+.header{
+
+    width: 100%;
+    height: 50px;
+    position: relative;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    
+}
+.logo{
+    margin-right: auto;
+    padding-left: 50px;
+}
+
+a{
+    text-decoration: none;
+    color: white;
+    padding-left: 20px;
+   
+}
+.nav{
+
+}
+.nav button{
+    
+    margin-right: 50px;
+    background: transparent;
+    border: 2px solid white;
+    border-radius: 5px;
+    padding-left:20px;
+    padding-right: 20px;
+    padding-top: 5px;
+    color: white;
+    
+}
+.regitration{
+    display: none;
+}
+
+
+form{
+   
+    padding: 40px;
+    border-radius: 15px;
+    background-color: rgba(107, 107, 131, 0.555);
+    
+
+}
+.inputs-text{
+    display:grid;
+    gap: 10px;
+}
+
+input{
+    background: transparent;
+    border-bottom: 5px solid black;
+}
+
+form button{
+    background: rgb(3, 3, 39);
+    color: white;
+    width: 100%;
+    border-radius: 10px;
+    height: 40px;
+    font-weight: 600;
+}
